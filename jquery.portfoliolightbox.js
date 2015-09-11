7825270c8e17bfc74ee62e643178997d78891f94
Paul Wells <pwellsau@gmail.com>
2015-09-11 16:45:55 +1000

    3rd commit - end of week

--- jquery.portfoliolightbox.js
+++ jquery.portfoliolightbox.js
@@ -15,15 +15,14 @@
         // I have used '.on' here, and not the shortcut '.click', incase someone creates images dynamically
         this.on("click", function () {
         	//alert("hello");
-        	$("body").append("<div id='#lightboxBackground'></div>");
-        	$("#lightboxBackground").css({'display':'block', 'background':'red'});
-        	//$("#lightboxBackground").fadeIn();
+        	$("body").append("<div id='lightboxBackground'></div>");
+        	//$("#lightboxBackground").css({'display':'block', 'background':'red'});
+        	$("#lightboxBackground").fadeIn();
         });
 
-        //$('body').on("click", '#lightboxBackground', function () {
-			//$("#lightboxBackground").fadeOut();
-			//alert("hello there");
-		//});
+        $('body').on("click", '#lightboxBackground', function () {
+			$("#lightboxBackground").remove();
+		});
 
     };
 
