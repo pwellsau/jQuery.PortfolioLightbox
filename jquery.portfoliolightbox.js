c0007b6a52ea69eec8bd15942b86273ce2b8edb5
Paul Wells <pwellsau@gmail.com>
2015-09-11 15:35:35 +1000

    2nd commit

---
+++ jquery.portfoliolightbox.js
@@ -0,0 +1,31 @@
+/*
+ * name, author, version, license. instructions?
+*/
+
+
+// Creating an Immediately invoked function expression.
+// Passing in 'jQuery' as param '$', to protect '$' in other libraries.
+// And also to keep our variables private.
+(function($) {
+
+	// Add our function to $.fn.
+    $.fn.lightBox = function() {
+
+        // My plugin.
+        // I have used '.on' here, and not the shortcut '.click', incase someone creates images dynamically
+        this.on("click", function () {
+        	//alert("hello");
+        	$("body").append("<div id='#lightboxBackground'></div>");
+        	$("#lightboxBackground").css({'display':'block', 'background':'red'});
+        	//$("#lightboxBackground").fadeIn();
+        });
+
+        //$('body').on("click", '#lightboxBackground', function () {
+			//$("#lightboxBackground").fadeOut();
+			//alert("hello there");
+		//});
+
+    };
+
+// Passing the function 'jQuery' as '$' parameter.
+}(jQuery));
