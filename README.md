01d8c4f86feb8feaa21036a336802c557912dada
Paul Wells <pwellsau@gmail.com>
2015-09-18 15:55:39 +1000

    Ready for testing

--- README.md
+++ README.md
@@ -1,16 +1,40 @@
 # Portfolio Lightbox jQuery Plugin #
 
-Simple jQuery plugin which will display a preview of your webpage in a lightbox, and include a description and links.
+Simple jQuery plugin which will display a preview of your image in a lightbox, and include a description and links.
 
 ##Version##
 0.1.0
 
 ##Usage##
 
-    <script>
-		// Instructions here
-		// Further instructions yo
-    </script>
+1. Ensure you have jQuery installed.
+2. Link to the plugin in your html document with
+
+        <script src="jQuery.PortfolioLightbox/jquery.portfoliolightbox.js"></script>
+
+3. Link to the CSS with
+
+        <link rel="stylesheet" href="jQuery.PortfolioLightbox/portfoliolightbox.css">
+
+4. Ensure your img element has an id attribute.
+5. Inside your script tags, configure the Portfolio Lightbox and it's settings as follows:
+
+        <script>
+		$(document).ready(function () {
+			$("#your-id").lightBox({
+				imgpath: 'path/to/yourimage.jpg',
+				title: 'Your Title',
+				text: 'Enter any text',
+				link: ['http://www.link1.com', 'http://www.link2.com'],
+				linkText: ['Site 1 Text', 'Link 2 text']
+			});		
+		});
+		</script>
+
+Create this for each id in each img element you want displayed in a lightbox.
+
+The 'imgpath' option is mandatory. The title and text options will display with default text if not entered, and specifying links are optional. You may enter as many links as you wish. Enter links between ' ' and separate by commas.
+
 
 ## Vendors ##
 jQuery - [http://jquery.com](http://jquery.com)
--- jquery.portfoliolightbox.js
+++ jquery.portfoliolightbox.js
@@ -1,35 +1,69 @@
 /*
- * name, author, description, version, examples, license. instructions?
+ *	Portfolio Lightbox plugin - by Paul Wells pwellsau(at)gmail.com
+ *	Displays your images in a lightbox with a title, description, and links.
+ * 	Version 0.1.0
+ *	See https://github.com/pwellsau/jQuery.PortfolioLightbox for README and instructions.
  */
 
 
 // Creating an Immediately invoked function expression.
 // Passing in 'jQuery' as param '$', to protect '$' in other libraries.
-// And also to keep our variables private.
+// Also helps to keep our variables private.
 (function($) {
 
 	// Add our function to $.fn.
     $.fn.lightBox = function(options) {
 
+    	// Set default options.
+    	// This merges with the options specified by the user when setting up the plugin.
+    	// Merges into the jQuery object as target argument (first argument) is omitted.
     	var settings = $.extend({
+    		imgpath		:null,
 			title		:'My Picture',
-			text		:'My description goes here',
-			link 		:null
+			text		:'Description goes here! See the readme for how to change this text.',
+			link 		:null,
+			linkText	:null,
+			addLinks	:function () {
+				// Function to gather URLs and their text, and format them.
+				completeLinks = ""
+				if (this.link == null) { 
+					// No links specified in options. Exit this function.
+					return completeLinks;
+				} else {
+					$.each(this.link, function(index, value) {
+						// If a link is specified but no accompanying text, use the URL as the text.
+						if (typeof settings.linkText[index] === 'undefined') {
+							var completeLinkText = value;
+						} else {
+							var completeLinkText = settings.linkText[index];
+						}
+						completeLinks = completeLinks + "<a href='" + value + "'>" + completeLinkText + "</a><br />";
+					});
+				}
+				return completeLinks;
+			}
 		}, options);
 
-        // My plugin.
         // I have used '.on' here, and not the shortcut '.click', incase someone creates images dynamically
         this.on("click", function () {
-        	//alert($(this).attr('src'));
-        	var imgsrc = $(this).attr('src');
+        	
+        	// Validation
+        	if (settings.imgpath == null) {
+        		console.log("You need to specify the 'imgpath' option, and add a path to your image for lightBox to work. Please see the readme file.");
+        		return this;
+        	};
+
+        	// Create html for the image, text and links
         	$("body").append("<div id='lightboxBackground'></div>" +
-        						"<div id='displayContainer'><div id='imgContainer'><img src='" + imgsrc + "'></div>" +
-        						"<div id='textContainer'><h2>" + settings.title + "</h2><p>" + settings.text + "</p></div><div class='break'></div></div>");
-        	//$("#lightboxBackground").css({'display':'block', 'background':'red'});
+        						"<div id='displayContainer'><div id='imgContainer'><img src='" + settings.imgpath + "'></div>" +
+        						"<div id='textContainer'><h2>" + settings.title + "</h2><p>" + settings.text + "</p>" + settings.addLinks() + "</div><div class='break'></div></div>");
+        	
+        	// Display lightbox
         	$("#lightboxBackground").fadeIn();
         	$("#displayContainer").fadeIn();
         });
 
+        // Click anywhere on the background to remove lightbox.
         $('body').on("click", '#lightboxBackground', function () {
 			$("#lightboxBackground").remove();
 			$("#displayContainer").remove();
